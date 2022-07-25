0369f79b0d6f4ed82d300fa4fb57e29555583c7f
Eddmafra <eduardo_mafra@hotmail.com>
2022-07-25 20:05:25 -0300

    for/in e for/of de fixação

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-funcoes/fixar4.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-funcoes/fixar4.js
@@ -1,19 +1,38 @@
-let name = 'Marta';
-let lastName = 'Silva';
-let age = 34;
-let medals = { golden: 2, silver: 3 };
+// let name = 'Marta';
+// let lastName = 'Silva';
+// let age = 34;
+// let medals = { golden: 2, silver: 3 };
 
-let footballer = {
-	name: 'Marta',
-	lastName: 'Silva',
-	age: 34,
-	medals: {
-		golden: 2,
-		silver: 3
-	},
-	bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
+// let footballer = {
+// 	name: 'Marta',
+// 	lastName: 'Silva',
+// 	age: 34,
+// 	medals: {
+// 		golden: 2,
+// 		silver: 3
+// 	},
+// 	bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
+// }
+
+// console.log ('A jogadora ' + footballer.name + ' ' + footballer.lastName + ' tem ' + footballer.age + ' anos de idade.');
+
+// console.log('A jogadora ' + footballer.name + ' ' + footballer.lastName + ' foi eleita a melhor do mundo por ' + footballer.bestInTheWorld.length + ' vezes.');
+
+// let names = {
+//   person1: 'João',
+//   person2: 'Maria',
+//   person3: 'Jorge',
+// };
+
+// for (let person in names) {
+//   console.log ('Olá, ' + names[person]);
+// }
+
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+for (key in car){
+  console.log(key + ' : ' + car[key]);
 }
-
-console.log ('A jogadora ' + footballer.name + ' ' + footballer.lastName + ' tem ' + footballer.age + ' anos de idade.');
-
-console.log('A jogadora ' + footballer.name + ' ' + footballer.lastName + ' foi eleita a melhor do mundo por ' + footballer.bestInTheWorld.length + ' vezes.');
